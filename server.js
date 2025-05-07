507a14ce696638b224f3484971a3a64c22d303a7
Aslam <rks507469@gmail.com>
2025-05-08 00:18:49 +0530

    added basic code to run server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-import './src/config/env';
+import './src/config/env.js';
+import express from 'express';
+import logger from './src/utils/logger.js';
+
+logger.info(`${process.env.PORT}`);
+const port = process.env.PORT || 9000;
+
+const app = express();
+
+app.listen(port, () => {
+    logger.info(`Server running on the port ${port}`);
+});
