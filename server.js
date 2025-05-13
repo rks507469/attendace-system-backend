81fd59769538ce93db91ae85bcdd87fc940bbd0c
Aslam <rks507469@gmail.com>
2025-05-14 01:57:55 +0530

    added message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import expressApplication from './src/app.js';
 const port = process.env.PORT || 9000;
 
 expressApplication.get("/", (req, res) => {
-    res.json({message: "Attendace System is UP!"});
+    res.json({message: "Attendace Backend System is UP!"});
 });
 
 expressApplication.listen(port, () => {
