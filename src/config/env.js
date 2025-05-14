bd11e02ab1596c83fa70589f3d1252c9fa7e3150
Aslam <rks507469@gmail.com>
2025-05-14 15:38:16 +0530

    stop requiring .env.example in production 2

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv-safe';
 import dotenvExpand from 'dotenv-expand';
 
 // Don't use any .env.example in production
-const isProduction = process.env.PRO_ENV === 'PRD';
+// const isProduction = process.env.PRO_ENV === 'PRD';
 
 
 const options = {
@@ -10,12 +10,12 @@ const options = {
     path: '.env',
 };
 
-if (!isProduction) {
-    options.example = '.env.example';
-}
+// if (!isProduction) {
+//     options.example = '.env.example';
+// }
 
-console.log('[ENV] isProduction:', isProduction);
-console.log('[ENV] config options:', options);
+// console.log('[ENV] isProduction:', isProduction);
+// console.log('[ENV] config options:', options);
 
 const env = dotenv.config(options);
 dotenvExpand.expand(env);
