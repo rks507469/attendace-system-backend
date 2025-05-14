b91fb61366c56fdca61d3b4e1277993c357709c5
Aslam <rks507469@gmail.com>
2025-05-14 14:04:07 +0530

    added file logging method

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import logger from './src/utils/logger.js';
 import expressApplication from './src/app.js';
+import fs from 'fs';
 
 const port = process.env.PORT || 9000;
 
 expressApplication.get("/", (req, res) => {
-    res.json({message: "Attendace Backend System is UP!"});
+    res.json({message: "Attendance Backend System is UP!"});
 });
+
+
+fs.readdirSync('./src/models').forEach(file => {
+    console.log('Found model file:', file);
+});
+
 
 expressApplication.listen(port, () => {
     logger.info(`Server running on the port ${port}`);
