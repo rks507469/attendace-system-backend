71c1a12909e5ef8fd5401327ca6acfa7a2d95b7e
Aslam <rks507469@gmail.com>
2025-05-18 00:28:25 +0530

    added speed insights

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import './models/student.js';
 import './models/attendance.js';
 import router from './routes/routeIndex.js';
 import cors from 'cors';
+import {injectSpeedInsights} from '@vercel/speed-insights';
 
 // Initialize express Application
 const expressApplication = express();
@@ -24,5 +25,8 @@ expressApplication.use('/api', router);
 
 // Connecting Database
 await connectDB();
+
+// speed insights
+injectSpeedInsights();
 
 export default expressApplication;
