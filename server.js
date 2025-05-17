fcc1f509017373eb554c090dc14bbf6dc4b7d0c4
Aslam <rks507469@gmail.com>
2025-05-18 01:38:24 +0530

    added external links for js and css

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ import logger from './src/utils/logger.js';
 import expressApplication from './src/app.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
-import {injectSpeedInsights} from "@vercel/speed-insights";
 
 
-// CDN CSS
-// const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.21.0/swagger-ui.min.js';
 // swagger configs
 const swaggerDefinition = {
     openapi: '3.0.0', info: {
@@ -58,18 +55,7 @@ const port = process.env.PORT || 9000;
  *                   example: Attendance Backend System is UP!
  */
 expressApplication.get("/", (req, res) => {
-    //res.json({message: "Attendance Backend System is UP!"});
-    res.send(`
-    <html lang="en">
-           <head>
-             <title>Attendance Backend System</title>
-           </head>
-           <body>
-             <h1>Attendance Backend System is UP!</h1>
-             ${injectSpeedInsights()}
-           </body>
-         </html>
-    `);
+    res.json({message: "Attendance Backend System is UP!"});
 });
 
 // Swagger for express
